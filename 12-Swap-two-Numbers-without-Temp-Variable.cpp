38a4772235d527cb70322b7c164dffdbb9def86a
BioEggsHD <alisup221@gmail.com>
2024-11-16 14:32:50 +0100

    Create 12-Swap-two-Numbers-without-Temp-Variable.cpp

---
+++ 12-Swap-two-Numbers-without-Temp-Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a = 5, b = 10;
+
+    std::cout << "Before swapping: a = " << a << ", b = " << b << endl;
+  
+    a = a ^ b;  // a becomes 15 (0101 ^ 1010 = 1111)
+    b = a ^ b;  // b becomes 5 (1111 ^ 1010 = 0101)
+    a = a ^ b;  // a becomes 10 (1111 ^ 0101 = 1010)
+
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
